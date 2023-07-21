32e13959d0fff3f911c727792f79d006b6609fbd
Elena <elenela@yandex.ru>
2023-07-21 15:28:59 +0300

    ДЗ 16

---
+++ src/main/java/com/sky/HW_16_algorithms/Hw16AlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.sky.HW_16_algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw16AlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw16AlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/HW_16_algorithms/Hw16AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.HW_16_algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw16AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
